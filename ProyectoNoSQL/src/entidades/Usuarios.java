daa7be36e4f9800a399eef33673c5e17793902b0
Kenway1526 <143423409+Kenway1526@users.noreply.github.com>
2025-09-16 14:31:11 -0600

    Add files via upload

---
+++ ProyectoNoSQL/src/entidades/Usuarios.java
@@ -0,0 +1,101 @@
+package entidades;
+/**
+ *
+ * @author Jonathan Esquivel Flores
+ */
+public class Usuarios 
+{
+    private int noUsuario;
+    private String nombre;
+    private String apPat;
+    private String apMat;
+    private String tcorreo;
+    private String curp;
+    private String rfc;
+    private int telefono;
+    private String idDepto;
+    private String sexo;
+    private String estatus;
+
+    public Usuarios(int idEmpleado, String nombre, String priAp, String secAp, String tcorreo, String curp, String rfc, int telefono, String idDepto, String sexo, String estatus) 
+    {
+        this.noUsuario = idEmpleado;
+        this.nombre = nombre;
+        this.apPat = priAp;
+        this.apMat = secAp;
+        this.tcorreo = tcorreo;
+        this.curp = curp;
+        this.rfc = rfc;
+        this.telefono = telefono;
+        this.idDepto = idDepto;
+        this.sexo = sexo;
+        this.estatus = estatus;
+    }
+    
+    public Usuarios()
+    {
+        
+    }
+
+    public int getNoUsuario() {
+        return noUsuario;
+    }
+    public void setNoUsuario(int noUsuario) {
+        this.noUsuario = noUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApPat() {
+        return apPat;
+    }
+    public void setApPat(String apPat) {
+        this.apPat = apPat;
+    }
+
+    public String getApMat() {
+        return apMat;
+    }
+    public void setApMat(String apMat) {
+        this.apMat = apMat;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String genero) {
+        this.sexo = genero;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+    
+    @Override
+    public String toString() {
+        return "Empleado{" + 
+       "noUsuario=" + noUsuario + 
+       ", nombre=" + nombre + 
+       ", apPat=" + apPat + 
+       ", apMat=" + apMat + 
+       ", tcorreo=" + tcorreo + 
+       ", curp=" + curp + 
+       ", rfc=" + rfc + 
+       ", telefono=" + telefono + 
+       ", idDepto=" + idDepto + 
+       ", sexo=" + sexo + 
+       ", estatus=" + estatus + 
+       '}';
+
+    }
+    
+    
+}
